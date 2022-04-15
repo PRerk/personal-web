71122a8655b8e95e72b67ea0b6b42fce93fc4f95
paris-jetcommerceth <paris.r@jetcommerce.co.th>
2022-04-15 15:51:49 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -219,7 +219,7 @@ export default {
               <img
                 width="100%"
                 style="height: 80%; width: 80%; position: relative; z-index: -1"
-                :src="gifUrl + '?' + String(Date.now()).slice(-2)"
+                :src="gifUrl + '?' + String(Date.now()).slice(0, -3)"
                 alt="Dashboard"
               />
             </div>
