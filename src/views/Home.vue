07f5bfa3b9400b8595350885917680d161adede3
paris-jetcommerceth <paris.r@jetcommerce.co.th>
2022-04-15 15:27:35 +0700

    FIx gif bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -219,7 +219,7 @@ export default {
               <img
                 width="100%"
                 style="height: 80%; width: 80%; position: relative; z-index: -1"
-                :src="gifUrl + '?' + Date.now()"
+                :src="gifUrl + '?' + String(Date.now()).slice(-2)"
                 alt="Dashboard"
               />
             </div>
